16a1227d6054a3143776f61cfd117db37ad7dd90
David <davidshevnya@ukr.net>
2026-01-17 16:28:13 +0000

    change config

--- app/config.py
+++ app/config.py
@@ -1,16 +1,17 @@
 import os
 
-#SQLALCHEMY
-SQLALCHEMY_ENGINES = {
-    'default': os.getenv('DATABASE_URI', 'postgresql://username:password@localhost/dbname')
-}
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# ALembic
+# Alembic
 ALEMBIC = {
     'script_location': '../migrations'
 }
 
 #JWT
+JWT_ACCESS_TOKEN_EXPIRES = 1200 # 20 minutes
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret')
 JWT_VERIFY_SUB = False
+
+#SQLALCHEMY
+SQLALCHEMY_ENGINES = {
+    'default': os.getenv('DATABASE_URI', 'postgresql://username:password@localhost/dbname')
+}
+SQLALCHEMY_TRACK_MODIFICATIONS = False
